a1d275c30e9b9bfb2a54bc4e2afbf055e3f6913f
Becca <beccarosenthal@gmail.com>
2017-10-05 05:25:40 +0000

    MVP one function

--- produce_summary.py
+++ produce_summary.py
@@ -15,12 +15,8 @@ def turn_files_into_pretty_text(text_files):
             line = line.rstrip()
             if line not in list_of_all_lines:
                 list_of_all_lines.append(line)
-    return list_of_all_lines
 
-
-def prints_pretty_data(list_of_strings):
-    """takes list of strings, splits it, prints pretty looking data""" 
-    for item in list_of_strings:
+    for item in list_of_all_lines:
 
         words = item.split('|')
         melon = words[0]
@@ -29,7 +25,7 @@ def prints_pretty_data(list_of_strings):
 
         print "Delivered {} {}s for total of ${}".format(count, melon, amount)
 
-prints_pretty_data(turn_line_into_list(all_files))
+turn_files_into_pretty_text(all_files)
 
 ### There are two ways to write this--as one
 
